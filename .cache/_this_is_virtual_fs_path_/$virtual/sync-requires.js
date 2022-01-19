4a5cb349a1a40b5435e76ed45218d3cedc689566
Alan Alvarado Salgado <alvarade.alain@gmail.com>
2022-01-18 21:13:35 -0800

    first

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\alvar\\dev\\gatsby\\gatsby-starter-portfolio-cara\\.cache\\dev-404-page.js")),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("C:\\Users\\alvar\\dev\\gatsby\\gatsby-starter-portfolio-cara\\node_modules\\@lekoarts\\gatsby-theme-cara\\src\\templates\\cara.tsx")),
+  "component---src-pages-404-jsx": preferDefault(require("C:\\Users\\alvar\\dev\\gatsby\\gatsby-starter-portfolio-cara\\src\\pages\\404.jsx"))
+}
+
